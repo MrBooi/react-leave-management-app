978a2129b4334716def6a69f8c8ef4d887dff447
Mr Booi <ayabongabooi2@gmail.com>
2018-11-12 11:45:18 +0200

    Updates

---
+++ precache-manifest.a144e791a0bbfcc3ab475537af50dc9c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-leave-management-app/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-leave-management-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-leave-management-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-leave-management-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-leave-management-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "147330b5e93927dd47a2",
+    "url": "/react-leave-management-app/static/js/runtime~main.147330b5.js"
+  },
+  {
+    "revision": "a5663e12df276708aeb5",
+    "url": "/react-leave-management-app/static/js/main.a5663e12.chunk.js"
+  },
+  {
+    "revision": "4fd91c74e51506835aa9",
+    "url": "/react-leave-management-app/static/js/1.4fd91c74.chunk.js"
+  },
+  {
+    "revision": "a5663e12df276708aeb5",
+    "url": "/react-leave-management-app/static/css/main.da11a117.chunk.css"
+  },
+  {
+    "revision": "fe7ca686a78a7f6203130a4bd345c7cf",
+    "url": "/react-leave-management-app/index.html"
+  }
+];
